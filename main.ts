e5336eb9e162ff7f3deabad36a998cd2ea5d0251
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2024-07-16 12:35:22 -0400

    fix: remove extra space

--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
 export default function greeter(lastName: string, firstName: string) {
-    console.log(`Yo,  ${firstName} ${lastName}. What's up?`)
+    console.log(`Yo, ${firstName} ${lastName}. What's up?`)
 }
