1a7cedcb4fc2be1f57e7f93cd161322bbbc3fa39
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2024-07-16 12:58:57 -0400

    feat: add want-to-play invitation

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 export default function greeter(lastName: string, firstName: string) {
     console.log(`Yo, ${firstName} ${lastName}. What's up?`)
     console.log(`Time is ${new Date().toISOString()}`)
+    console.log(`Do you want to play?`)
 }
