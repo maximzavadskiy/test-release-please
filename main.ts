4e030713adbe83c65b1054c104691567dfd399ae
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2024-07-16 12:53:36 -0400

    feat: output time

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
 export default function greeter(lastName: string, firstName: string) {
     console.log(`Yo, ${firstName} ${lastName}. What's up?`)
+    console.log(`Time is ${new Date().toISOString()}`)
 }
