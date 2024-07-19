fca8df95da5dc60f4ed3c2532128847d45fdc3c7
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2024-07-19 14:53:36 -0400

    Revert "fix: politness everywhere"
    This reverts commit fd38f8d38c40700f4aea7caec98dbea3d1c845f2.

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 export default function greeter(lastName: string, firstName: string) {
     console.log(`Dear ${firstName} ${lastName}.What's up?`)
     console.log(`Time is ${new Date().toISOString()}`)
-    console.log(`Do you want to play, kind sir?`)
+    console.log(`Do you want to play?`)
 }
