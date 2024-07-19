b95467c3b3ea246b13c80274c1bbe1368a86cf98
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2024-07-19 14:58:25 -0400

    feat: add encouragment

--- main.ts
+++ main.ts
@@ -2,4 +2,5 @@ export default function greeter(lastName: string, firstName: string) {
     console.log(`Dear ${firstName} ${lastName}.What's up?`)
     console.log(`Time is ${new Date().toISOString()}`)
     console.log(`Do you want to play?`)
+    console.log(`I'd really love to!`)
 }
