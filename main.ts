3ebe071164d29629875d68d3c4b6815c6895c80d
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2024-08-02 16:46:00 -0400

    feat: add weather forecast

--- main.ts
+++ main.ts
@@ -3,4 +3,5 @@ export default function greeter(lastName: string, firstName: string) {
     console.log(`Time is ${new Date().toISOString()}`)
     console.log(`Do you want to play?`)
     console.log(`I'd really love to!`)
+    console.log(`Its currenlty sunny.`)
 }
