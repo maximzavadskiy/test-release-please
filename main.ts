6884da75ad2973815c03266f2e0fcd2bc6e925d6
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2024-08-02 17:14:14 -0400

    feat: add humidity status

--- main.ts
+++ main.ts
@@ -4,4 +4,5 @@ export default function greeter(lastName: string, firstName: string) {
     console.log(`Do you want to play?`)
     console.log(`I'd really love to!`)
     console.log(`Its currenlty sunny.`)
+    console.log(`Humidity 80%`)
 }
