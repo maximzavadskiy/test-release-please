3cceb55093c4989b0ec9ff43a0864c43cb3203ae
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2024-08-16 15:29:16 +0300

    feat: remove joke

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 export default function greeter(lastName: string, firstName: string) {
     console.log(`Dear ${firstName} ${lastName}.What's up?`)
     console.log(`Time is ${new Date().toISOString()}`)
-    console.log(`Let me tell you a joke?`)
 }
